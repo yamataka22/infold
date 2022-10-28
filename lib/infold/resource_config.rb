08305c6fc57fe5a2fe30cc5856a6a527bc0f447a
takaharu yamamoto <yamataka22@gmail.com>
2022-10-29 08:08:50 +0900

    refactoring resource_config.form_associations

--- lib/infold/resource_config.rb
+++ lib/infold/resource_config.rb
@@ -25,13 +25,11 @@ module Infold
 
     def form_associations
       return @form_associations if @form_associations
-      @form_associations = []
-      form_fields = @setting.app&.form&.fields&.map{ |k,v| value_or_mash_key(k) }
-      model_has_many = @setting.model&.associations&.has_many&.map{ |k,v| value_or_mash_key(k) }
-      @form_associations += form_fields&.select { |name| model_has_many.include?(name)  }.to_a if model_has_many
-      model_has_one = @setting.model&.associations&.has_one&.map{ |k,v| value_or_mash_key(k) }
-      @form_associations += form_fields&.select { |name| model_has_one.include?(name)  }.to_a if model_has_one
-      @form_associations
+      form_fields = @setting.app&.form&.fields&.map { |k,v| value_or_mash_key(k) }
+      model_associations =
+        @setting.model&.associations&.has_many&.map { |k,v| value_or_mash_key(k) }.to_a +
+        @setting.model&.associations&.has_one&.map  { |k,v| value_or_mash_key(k) }.to_a
+      @form_associations = form_fields&.select { |form_field| model_associations.include?(form_field)  }.to_a if model_associations
     end
 
     def active_storages
