a83d8c6f79de104e88590fef1d9b66ef3c6f156c
takaharu yamamoto <yamataka22@gmail.com>
2022-11-26 09:38:08 +0900

    fix dummy/routes.rb

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  draw(:admin)
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
